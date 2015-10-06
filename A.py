4a3f69e20cf28d2c3d8af14a2378bf3286803740
chelberserker <bersenjow@gmail.com>
2015-10-06 11:22:40 +0300

    add new files

--- A.py
+++ A.py
@@ -4,9 +4,9 @@ for i in range(n):
 	A[i]=int(A[i])
 A.append(' ')	
 for i in range(n):
-	for j in range(i, n):
-		if A[i+1]==A[j]: 
-				el=A[i]
-print(el)
+	for j in range(i+1, n):
+		if A[i]==A[j]:
+			print(A[i])
+			break
 
 	
