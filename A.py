2741048b626496ab6f3f6c2fa0f2c7d2d11eb515
chelberserker <bersenjow@gmail.com>
2015-12-04 13:43:31 +0300

    Update A.py

--- A.py
+++ A.py
@@ -1,14 +1,9 @@
-input = open('input.txt', 'r')
-output = open('output.txt', 'w')
-n = input.read()
-A = input().split()
-for i in range(n):
-	A[i]=int(A[i])
-A.append(' ')	
-for i in range(n):
-	for j in range(i+1, n):
-		if A[i]==A[j]:
-			output.write(A[i])
-			break
-output.close()
-	
+with open('input.txt', 'r') as inp:
+    with open('output.txt', 'w') as out:
+        N = int(inp.readline())
+        lis = inp.readline().split()
+        lis.sort()
+        for i in range(N-1):
+            if lis[i] == lis[i+1]:
+                print(lis[i], file=out)
+                break
